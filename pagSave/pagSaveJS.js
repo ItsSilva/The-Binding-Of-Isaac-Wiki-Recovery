345bd889a5a26e65e52f51aae763390cea4bc1ed
Juan S Silva <juanse909@hotmail.com>
2024-03-19 09:14:50 -0500

    save change px to rems

---
+++ pagSave/pagSaveJS.js
@@ -0,0 +1,21 @@
+// Drop-down Profile
+let subMenu = document.getElementById("subMenu");
+
+function toggleMenu(){
+    subMenu.classList.toggle("open-menu");
+};
+// Drop-down Profile
+
+// savecolor
+let saveColors = document.querySelectorAll(".savecolor");
+
+function toggleClass(element, text) {
+  element.classList.toggle(text);
+}
+
+saveColors.forEach(function(saveColor) {
+  saveColor.addEventListener("click", function() {
+    toggleClass(saveColor, "active");
+  });
+});
+// savecolor
