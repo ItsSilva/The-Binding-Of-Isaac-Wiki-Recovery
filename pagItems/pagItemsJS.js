156420f3516893d61d118b404189cadfe95ed768
Juan S Silva <juanse909@hotmail.com>
2024-04-28 14:03:46 -0500

    pagItemsJS.js burger menu working (?)

--- pagItems/pagItemsJS.js
+++ pagItems/pagItemsJS.js
@@ -1,27 +1,51 @@
-import { obtenerData,  pagItems} from '../data/utils.js';
+import { obtenerData, pagItems } from '../data/utils.js';
 
 const render = async () => {
+  // class
+  const data = await obtenerData();
 
-//class
-const data = await obtenerData();
+  const sizeTextinfoCards = document.querySelector('.tableContainer__infoCards > tbody');
 
-const sizeTextinfoCards = document.querySelector('.tableContainer__infoCards > tbody');
-
-for (const achievement of data.pagItems){
-  const pageAchievements = new pagItems(
-    achievement.id,
-    achievement.name, 
-    achievement.itemID, 
-    achievement.icon, 
-    achievement.quote,
-    achievement.description, 
-    achievement.quality
+  for (const achievement of data.pagItems) {
+    const pageAchievements = new pagItems(
+      achievement.id,
+      achievement.name,
+      achievement.itemID,
+      achievement.icon,
+      achievement.quote,
+      achievement.description,
+      achievement.quality
     );
-  const pageAchievementsRender = pageAchievements.render();
+    const pageAchievementsRender = pageAchievements.render();
 
-  sizeTextinfoCards.appendChild(pageAchievementsRender);
-}
+    sizeTextinfoCards.appendChild(pageAchievementsRender);
+  }
 
+  // Drop-down Profile
+  let subMenu = document.getElementById("subMenu");
+  let profileButton = document.querySelector('.user-pic');
+
+  if (profileButton) {
+    profileButton.addEventListener('click', toggleMenu);
+  }
+
+  function toggleMenu() {
+    subMenu.classList.toggle("open-menu");
+  }
+
+  // savecolor
+  let saveColors = document.querySelectorAll(".tableContainer__infoCards--savecolor");
+
+  function toggleClass(element, text) {
+    element.classList.toggle(text);
+  }
+
+  saveColors.forEach(function(saveColor) {
+    saveColor.addEventListener("click", function() {
+      toggleClass(this, "active");
+    });
+  });
+  // savecolor
 };
 
-document.addEventListener("DOMContentLoaded", render);
+render();
