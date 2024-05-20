895be6461afc079dc32122236328e94a3e18f969
Juan S Silva <juanse909@hotmail.com>
2024-05-19 19:36:06 -0500

    searchbar working in pagAchievementsJS.js

--- pagAchievements/pagAchievementsJS.js
+++ pagAchievements/pagAchievementsJS.js
@@ -1,31 +1,38 @@
-import { obtenerUsuarioEnSesion, logout } from '/data/session.js';
-import { obtenerData,  PageAchievements} from '../data/utils.js';
+import { obtenerUsuarioEnSesion, logout } from "/data/session.js";
+import { obtenerData, PageAchievements } from "../data/utils.js";
 
 const render = async () => {
+  //class
+  const data = await obtenerData();
 
-//class
-const data = await obtenerData();
+  const sizeTextinfoCards = document.querySelector(
+    ".tableContainer__infoCards > tbody"
+  );
 
-const sizeTextinfoCards = document.querySelector('.tableContainer__infoCards > tbody');
+  for (const achievement of data.pagAchievement) {
+    const pageAchievements = new PageAchievements(
+      achievement.id,
+      achievement.image,
+      achievement.name,
+      achievement.description,
+      achievement.unlock,
+      achievement.secretNumber
+    );
+    const pageAchievementsRender = pageAchievements.render();
 
-for (const achievement of data.pagAchievement){
-  const pageAchievements = new PageAchievements(achievement.id, achievement.image, achievement.name, achievement.description, achievement.unlock, achievement.secretNumber);
-  const pageAchievementsRender = pageAchievements.render();
-
-  sizeTextinfoCards.appendChild(pageAchievementsRender);
-}
-
-// Función de búsqueda
+    sizeTextinfoCards.appendChild(pageAchievementsRender);
+  }
+  // Función de búsqueda
 const searchInput = document.querySelector('.search-input');
 const searchRows = document.querySelectorAll('.tableContainer__infoCards tbody tr');
 
 searchInput.addEventListener('input', function() {
   const searchTerm = searchInput.value.toLowerCase();
   searchRows.forEach(function(row) {
-    const name = row.querySelector('td:nth-child(2)').textContent.toLowerCase();
-    const description = row.querySelector('td:nth-child(3)').textContent.toLowerCase();
-    const unlock = row.querySelector('td:nth-child(4)').textContent.toLowerCase();
-    const secretNumber = row.querySelector('td:nth-child(5)').textContent.toLowerCase();
+    const name = row.querySelector('td:nth-child(2)').textContent.toLowerCase(),
+      description = row.querySelector('td:nth-child(3)').textContent.toLowerCase(),
+      unlock = row.querySelector('td:nth-child(4)').textContent.toLowerCase(),
+      secretNumber = row.querySelector('td:nth-child(5)').textContent.toLowerCase();
     if (name.includes(searchTerm) || description.includes(searchTerm) || unlock.includes(searchTerm) || secretNumber.includes(searchTerm)) {
       row.style.display = 'table-row';
     } else {
@@ -34,13 +41,12 @@ searchInput.addEventListener('input', function() {
   });
 });
 
-
   // Drop-down Profile
   let subMenu = document.getElementById("subMenu");
-  let profileButton = document.querySelector('.user-pic');
+  let profileButton = document.querySelector(".user-pic");
 
   if (profileButton) {
-    profileButton.addEventListener('click', toggleMenu);
+    profileButton.addEventListener("click", toggleMenu);
   }
 
   function toggleMenu() {
@@ -48,34 +54,39 @@ searchInput.addEventListener('input', function() {
   }
 
   // savecolor
-  let saveColors = document.querySelectorAll(".tableContainer__infoCards--savecolor");
+  let saveColors = document.querySelectorAll(
+    ".tableContainer__infoCards--savecolor"
+  );
 
   function toggleClass(element, text) {
     element.classList.toggle(text);
   }
 
-  saveColors.forEach(function(saveColor) {
-    saveColor.addEventListener("click", function() {
+  saveColors.forEach(function (saveColor) {
+    saveColor.addEventListener("click", function () {
       toggleClass(this, "active");
     });
   });
   // savecolor
   const usuarioActivo = obtenerUsuarioEnSesion();
 
-// Importante para no perder nota.
-if (!usuarioActivo) {
-    window.location.href = '/index/index.html';
+  // Importante para no perder nota.
+  if (!usuarioActivo) {
+    window.location.href = "/index/index.html";
     return;
-};
+  }
 
-const usuarioActivoNombre = document.querySelector('#usuarioActivo');
-usuarioActivoNombre.innerHTML = 'Bienvenido ' + usuarioActivo.correo ;
+  const usuarioActivoNombre = document.querySelector("#usuarioActivo");
+  usuarioActivoNombre.innerHTML = "Bienvenido " + usuarioActivo.correo;
 
-const cerrarSesion = document.querySelector('#cerrarSesion');
-cerrarSesion.addEventListener('click', () => {
+  const cerrarSesion = document.querySelector("#cerrarSesion");
+  cerrarSesion.addEventListener("click", () => {
     logout();
-    window.location.href = '/index/index.html';
-});
+    window.location.href = "/index/index.html";
+  });
 };
 
 render();
+
+
+
